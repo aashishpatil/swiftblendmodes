c4838adc289ad31b5d2a17e892f8b48c55ed5a66
Aashish Patil <aashish.mobile.apps@gmail.com>
2025-10-23 17:23:56 -0700

    Initial Commit

---
+++ BlendModes/BlendModesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlendModesApp.swift
+//  BlendModes
+//
+//  Created by Aashish Patil on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BlendModesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
